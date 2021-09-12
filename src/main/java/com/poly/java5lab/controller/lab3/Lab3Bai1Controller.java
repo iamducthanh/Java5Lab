58293e40455e96f570fbc3585e9acc3e74fa499f
iamducthanh <ducthanh260801@gmail.com>
2021-09-12 21:48:16 +0700

    3

--- src/main/java/com/poly/java5lab/controller/lab3/Lab3Bai1Controller.java
+++ src/main/java/com/poly/java5lab/controller/lab3/Lab3Bai1Controller.java
@@ -4,7 +4,9 @@ import com.poly.java5lab.entity.Genre;
 import com.poly.java5lab.entity.ProductEntity;
 import com.poly.java5lab.entity.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,21 +19,22 @@ public class Lab3Bai1Controller {
     public String bai1Page(@ModelAttribute("student") Student student) {
         return "views/lab3/lab3bai1";
     }
+
     @ModelAttribute("genders")
-    public List<Genre> getGenders(){
-       List<Genre> genres = new ArrayList<>();
-       genres.add(new Genre(true, "Male"));
-       genres.add(new Genre(false, "Female"));
+    public List<Genre> getGenders() {
+        List<Genre> genres = new ArrayList<>();
+        genres.add(new Genre(true, "Male"));
+        genres.add(new Genre(false, "Female"));
         return genres;
     }
 
     @ModelAttribute("faculties")
-    public List<String> getFaculties(){
+    public List<String> getFaculties() {
         return Arrays.asList("CNTT", "DLNHKS", "QTDN");
     }
 
     @ModelAttribute("hobbies")
-    public List<String> getHobbies(){
+    public List<String> getHobbies() {
         List<String> hobbies = new ArrayList<>();
         hobbies.add("Travelling");
         hobbies.add("Music");
@@ -43,16 +46,24 @@ public class Lab3Bai1Controller {
     @PostMapping("/lab3/bai1")
     public String bai1Save(@Validated @ModelAttribute("student") Student student,
                            BindingResult result,
+                           Model model,
                            @RequestParam("lab3File") MultipartFile file) {
         System.out.println(file.getSize());
         System.out.println(student.toString());
         if (!result.hasErrors()) {
-            System.out.println("not error");
+            model.addAttribute("success", "Lưu thành công");
         } else {
-            System.out.println("error");
-            System.out.println(result.getSuppressedFields().toString());
+            ResourceBundle message = ResourceBundle.getBundle("message");
+            List<String> errorMessages = new ArrayList<>();
+            List<ObjectError> errors = result.getAllErrors();
+            for (ObjectError objectError : errors) {
+                errorMessages.add(message.getString(objectError.getDefaultMessage()));
+            }
+            Collections.reverse(errorMessages);
+            model.addAttribute("messages", errorMessages);
+            model.addAttribute("eee","eee");
         }
-        return "views/lab3/lab3bai1";
+        return"views/lab3/lab3bai1";
     }
-
 }
+
